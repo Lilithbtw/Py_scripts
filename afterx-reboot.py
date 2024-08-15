8517f69b72f586b23d76b24a7d66950af1f8e9c8
Lilithbtw <lili.macias@skiff.com>
2024-08-15 15:11:00 +0200

    Add files via upload

---
+++ afterx-reboot.py
@@ -0,0 +1,14 @@
+import psutil
+import os
+import time
+
+#check uptime
+def minutes_elapsed():
+    return (time.time() - psutil.boot_time())/60
+
+
+#if grater then 3 days reboot
+while True:
+    time.sleep(60)
+    if minutes_elapsed() >= 4320:
+        os.system('systemctl reboot -i')
